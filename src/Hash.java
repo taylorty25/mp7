523fe56af788c5181d5b2e36ced751f2e52c4c6a
rrrr4444 <reedcolloton@gmail.com>
2023-11-05 13:58:46 -0600

    equals

--- src/Hash.java
+++ src/Hash.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Hash Class
 public class Hash {
   byte[] data;
@@ -19,7 +21,7 @@ public class Hash {
    * Returns true if this hash meets the criteria for validity, i.e., its first three indices contain zeroes.
    */
   public boolean isValid(){
-    return data[0] == 0 && data[1] == 0 && data[2]==0;
+    return data[0] == 0 && data[1] == 0 && data[2] == 0;
   }
   /**
    * Returns the string representation of the hash as a string of hexadecimal digits, 2 digits per byte.
@@ -34,23 +36,11 @@ public class Hash {
   /**
    * Returns true if this hash is structurally equal to the argument.
    */
-  boolean equals(Object other) {
-    return this.toString().equals(other.toString());
+  public boolean equals(Object other) {
+    if (! (other instanceof Hash)) {
+      return false;
+    } // if
+    return Arrays.equals(this.getData(), ((Hash) other).getData());
   } // boolean equals(Object other)
 
 }
-
-//
-boolean isValid(): returns true if this hash meets the criteria for validity, i.e., its first three indices contain zeroes.
-String toString(): returns the string representation of the hash as a string of hexadecimal digits, 2 digits per byte.
-To implement toString(), you will find the following static methods useful:
-
-Byte.toUnsignedInt
-String.format
-The conversion method Byte.toUnsignedInt is necessary because all integral values in Java are signed. We do not want values past 127 to be interpreted as negative numbers, e.g., if we have the bit pattern 11111111, we want to interpret this as the value 255 not -128. String.format works like sprintf which acts like printf but writes its output to a string. You should use a format specifier that prints an integer to the screen in hexadecimal using two digits; read the documentation to discover what this format specifier is.
-
-The equals method should check to see if:
-
-other is an instance of Hash using the instanceof operator.
-If so, it should cast other to type Hash, e.g., Hash o = (Hash) other and then use the Arrays.equals static method to perform the appropriate equality check on the two Hash object’s arrays.
-*/
