d454b9d9bd20f08ba1dbeda87719bc2a205251dd
taylorty25 <144643861+taylorty25@users.noreply.github.com>
2023-11-05 13:18:41 -0600

    Update Hash.java
    update to the constructor, added isValid() and toString(), removed unused imports

--- src/Hash.java
+++ src/Hash.java
@@ -1,28 +1,12 @@
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-/**
- * Hash object. Allows creating and comparing hashes.
- * @author Tyrell
- * @author Reed
- */
-
-
-//Hash Class
-public class Hash{
-
-  // hash value
+// Hash Class
+public class Hash {
   byte[] data;
-
   /**
-   * Constructs a new Hash object that contains the given hash (as an array of bytes).
+   * constructs a new Hash object that contains the given hash (as an array of bytes).
    */
-  public static byte[] Hash(String msg) throws NoSuchAlgorithmException {
-    MessageDigest md = MessageDigest.getInstance("sha-256");
-    md.update(msg.getBytes());
-    byte[] hash = md.digest();
-    this.data = hash;
-  } // calculateHash(String)
+  public Hash(byte[] data) {
+    this.data = data;
+  }// calculateHash(String)
 
   /**
    * Returns the hash contained in this object.
@@ -31,6 +15,22 @@ public class Hash{
     return data;
   } // byte[] getData()
 
+  /**
+   * Returns true if this hash meets the criteria for validity, i.e., its first three indices contain zeroes.
+   */
+  public boolean isValid(){
+    return data[0] == 0 && data[1] == 0 && data[2]==0;
+  }
+  /**
+   * Returns the string representation of the hash as a string of hexadecimal digits, 2 digits per byte.
+   */
+  public String toString() {
+    StringBuilder hex = new StringBuilder();
+    for (byte b : data) {
+      hex.append(String.format("%02X ", Byte.toUnsignedInt(b)));
+    }
+    return hex.toString();
+  }
   /**
    * Returns true if this hash is structurally equal to the argument.
    */
